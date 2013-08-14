0a146863a09a47d3ece1a4e1271889311713b7c2
Titan <titanlien@ami.com.tw>
2013-08-15 06:18:28 +0800

    Initial Commit

---
+++ registerCallback/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  registerCallback
+//
+//  Created by titan on 8/15/13.
+//  Copyright (c) 2013 titan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

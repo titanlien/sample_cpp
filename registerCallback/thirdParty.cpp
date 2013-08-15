9d96bbc74c408485a1b0587de1263389e82561c8
Titan <titan.lien@gmail.com>
2013-08-15 14:55:39 +0800

    setup typedef for function pointer in thirdParty

---
+++ registerCallback/thirdParty.cpp
@@ -0,0 +1,16 @@
+//
+//  thirdParty.cpp
+//  registerCallback
+//
+//  Created by titan on 8/15/13.
+//  Copyright (c) 2013 titan. All rights reserved.
+//
+
+#include "thirdParty.h"
+
+bool
+register_fun_call(void fun(), void* data)
+{
+    
+    return true;
+}
---
+++ registerCallback/thirdParty.h
@@ -0,0 +1,14 @@
+//
+//  thirdParty.h
+//  registerCallback
+//
+//  Created by titan on 8/15/13.
+//  Copyright (c) 2013 titan. All rights reserved.
+//
+
+#ifndef __registerCallback__thirdParty__
+#define __registerCallback__thirdParty__
+
+#include <iostream>
+
+#endif /* defined(__registerCallback__thirdParty__) */
